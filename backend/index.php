05d7e28a599d47f66b7f002a572847ae99426c6d
Vyshnavi <vyshnavik429@gmail.com>
2025-12-25 12:54:12 +0530

    Fix backend root route and remove composer dependency

--- backend/index.php
+++ backend/index.php
@@ -1,9 +1,15 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
-
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 
 $articles = [
   [
